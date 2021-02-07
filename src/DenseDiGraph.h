7ad93fadb6b91222f2320d7bd401def0e2bb8ed4
Subhajit Sahu <wolfram77@gmail.com>
2021-02-07 14:23:39 +0530

    :bus: try DenseDiGraph

--- src/DenseDiGraph.h
+++ src/DenseDiGraph.h
@@ -1,28 +1,28 @@
 #pragma once
 #include "_support.h"
-#include <vector>
+#include <array>
 
 using namespace std;
 
 
-template <typename T>
+template <class T>
 class DenseDiGraph {
   public:
   int order;
-  vector<int> degrees;
-  vector<T>   weights;
+  int* degrees;
+  T*   weights;
 
   DenseDiGraph(int n) {
     order = n;
-    degrees
     degrees = new int[n];
-    weights = new float[n*n];
+    weights = new T[n*n];
   }
 
-  void addLink(int i, int j, float wt=1) {
+  void addLink(int i, int j, float w=1) {
     int n = order;
-    degrees[i] += wt               != 0? 1 : 0;
-    degrees[i] -= weights[j*n + i] != 0? 1 : 0;
-    weights[j*n + i] = wt;
+    T& w0 = &weights[j*n + i];
+    degrees[i] += w  != 0? 1 : 0;
+    degrees[i] -= w0 != 0? 1 : 0;
+    w0 = w;
   }
 };
--- src/main.h
+++ src/main.h
@@ -1,2 +1,3 @@
 #pragma once
 #include "sum.h"
+#include "DenseDiGraph.h"

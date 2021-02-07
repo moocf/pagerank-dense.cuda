922b486172a0a73df53ca3763d597497d57eb9df
Subhajit Sahu <wolfram77@gmail.com>
2021-02-07 12:48:29 +0530

    :bus: try include pageRank

--- src/pageRank.h
+++ src/pageRank.h
@@ -10,8 +10,8 @@
 // Finds rank of nodes in graph.
 void pageRank(float *a, DenseDiGraph x, float damping=0.85, float convergence=1e-5) {
   int n = x.order;
-  float *r0 = arrayFill(a, 1.0f/n);
-  float *r1 = arrayFill(malloc(n), 0);
+  float *r0 = arrayFill(a, n, 1.0f/n);
+  float *r1 = new float[n];
   do {
     int e = 0;
     for (int j=0; j<n; j++) {
